08627bf220dcea46f9234ab1e42067509ded07ba
Manoj Baskar <147849968+manojaba@users.noreply.github.com>
2024-11-15 07:37:38 +0530

    nav.js  updated

--- src/Flipkart/Description.js
+++ src/Flipkart/Description.js
@@ -2,6 +2,8 @@ export default function Description() {
     return (
         <>
             <h1>Description</h1>
+            <img src="e-commerce\src\assets\fkheaderlogo_exploreplus-44005d.svg"></img>
+
         </>
     )
 }
--- src/Flipkart/Navigation.js
+++ src/Flipkart/Navigation.js
@@ -1,7 +1,16 @@
 export default function Navigation() {
     return (
         <>
-            <h1>Navigation</h1>
+            <nav className="flex">
+                <div>
+                    <img src="e-commerce/src/assets/flipkart-logo.svg" alt='flipkart logo'></img>
+                </div>
+                <input type='text' placeholder="Seatch for Products,Brands and More"></input>
+                <select>
+                    <option>Login</option>
+                </select>
+                <button>Cart</button>
+            </nav>
         </>
     )
 }
